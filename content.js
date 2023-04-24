e0d6292966fb72735aec8b9ad38ec0ebe87b76c4
Kush Shah <kush5683@gmail.com>
2023-04-24 14:19:30 -0400

    YouTube updated the way the shorts are shown

--- content.js
+++ content.js
@@ -1,29 +1,38 @@
-browser.storage.onChanged.addListener((changes, areaName) => {
-  if (areaName === "local" && changes.value) {
-    console.log(changes.value.newValue);
-    if (!changes.value.newValue) {
-      window.location.reload();
+// Debounce function to limit the frequency of function calls
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+// Function to remove elements with the 'is-shorts' property
+function removeIsShortsElements() {
+  // Select all elements with the 'is-shorts' attribute
+  const elementsWithIsShorts = document.querySelectorAll("[is-shorts]");
+
+  // Iterate through each element and remove it
+  elementsWithIsShorts.forEach((element) => {
+    element.remove();
+  });
+}
+
+// Function to handle DOM mutations and call removeIsShortsElements()
+function handleDomMutations(mutations) {
+  for (let mutation of mutations) {
+    if (mutation.type === "childList") {
+      debouncedRemoveIsShortsElements();
     }
-    update(changes.value.newValue);
-  }
-});
-
-function update(value) {
-  if (!value) {
-    return;
-  } else {
-    var vids = document.querySelectorAll("ytd-grid-video-renderer");
-
-      for (let i = 0; i < vids.length; i++) {
-        if (vids[i].innerHTML.includes('aria-label="Shorts"')) {
-          vids[i].remove();
-        }
-      }
-    
   }
 }
 
-browser.storage.local.get("value").then((result) => console.log(result.value));
-setTimeout(function () {
-  browser.storage.local.get("value").then((result) => update(result.value));
-}, 1000);
+removeIsShortsElements();
+
+// Debounce removeIsShortsElements() with a 100ms delay
+const debouncedRemoveIsShortsElements = debounce(removeIsShortsElements, 100);
+
+// Observe DOM changes and call removeIsShortsElements() when necessary
+const observer = new MutationObserver(handleDomMutations);
+observer.observe(document.body, { childList: true, subtree: true });
