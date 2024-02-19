276e109661f4318665b07c3df6a2551fbdff55ce
Kush Shah <kshah2@wpi.edu>
2024-02-18 21:35:15 -0800

    moved things around to work with safari

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,21 @@
-console.log("Hello World!", browser);
+var hideShortsCheckBox = document.getElementById("hideShorts");
+
+hideShortsCheckBox.addEventListener("change", (e) => {
+  setValue(e.target.checked);
+});
+
+async function setValue(value) {
+  await browser.storage.local.set({ value });
+}
+
+async function init() {
+  let value;
+  browser.local.storage.get("value").then((result) => (value = result.value));
+  if (value === undefined) {
+    value = true;
+  }
+  hideShortsCheckBox.checked = value;
+  setValue(value);
+}
+
+init().catch((e) => console.error(e));
