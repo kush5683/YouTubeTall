cee0498fea128e4afefb6e1fdba0ca65888c79fc
Kush Shah <kshah2@wpi.edu>
2024-02-18 21:17:19 -0800

    added xcode stuff

---
+++ macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Kush Shah on 2/18/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
