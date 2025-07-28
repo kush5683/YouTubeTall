f0b8c64c823d40c93287347baf9ecd71e00878a3
Kush Shah <32845063+kush5683@users.noreply.github.com>
2025-07-28 09:29:56 -0700

    feat: migrate extension to manifest v3

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,12 +1,13 @@
 let totalHidden = 0;
+const actionApi = browser.action || browser.browserAction;
 
 browser.runtime.onMessage.addListener((request) => {
   if (request.type === "count") {
     totalHidden += request.count || 0;
-    browser.browserAction.setBadgeText({ text: totalHidden.toString() });
-    browser.browserAction.setBadgeBackgroundColor({ color: "#FF0000" });
+    actionApi.setBadgeText({ text: totalHidden.toString() });
+    actionApi.setBadgeBackgroundColor({ color: "#FF0000" });
   } else if (request.type === "reset") {
     totalHidden = 0;
-    browser.browserAction.setBadgeText({ text: "" });
+    actionApi.setBadgeText({ text: "" });
   }
 });
