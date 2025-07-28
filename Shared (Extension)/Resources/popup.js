573a3961f08cf2be77c192a309c765a69391d433
Kush Shah <32845063+kush5683@users.noreply.github.com>
2025-07-27 22:27:48 -0700

    Add user toggle and badge count

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,21 +1,23 @@
-var hideShortsCheckBox = document.getElementById("hideShorts");
+const hideShortsCheckBox = document.getElementById("hideShorts");
+const storage = browser.storage.sync || browser.storage.local;
 
 hideShortsCheckBox.addEventListener("change", (e) => {
   setValue(e.target.checked);
 });
 
 async function setValue(value) {
-  await browser.storage.local.set({ value });
+  await storage.set({ hideShorts: value });
+  await browser.runtime.sendMessage({ type: "preference", hideShorts: value });
 }
 
 async function init() {
-  let value;
-  browser.local.storage.get("value").then((result) => (value = result.value));
+  const result = await storage.get("hideShorts");
+  let value = result.hideShorts;
   if (value === undefined) {
     value = true;
   }
   hideShortsCheckBox.checked = value;
-  setValue(value);
+  await setValue(value);
 }
 
 init().catch((e) => console.error(e));
